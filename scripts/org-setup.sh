ec3cbb372e4ef5528af096f8e51a1a384115cde7
Nathan Pepper <MayTheSForceBeWithYou@gmail.com>
2019-01-04 09:28:22 -0800

    Updated scratch org definition to enable Chatter. Updates to setup script for error handling.

--- scripts/org-setup.sh
+++ scripts/org-setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "Please supply a scratch org alias"
+if [ $# -eq 0 ]; then
+	>&2 echo "No scratch org alias provided"
     exit 1
 fi
 
-echo "Creating scratch org with alias $1..."
-sfdx force:org:create -f ../config/project-scratch-def.json -s -a "$1"
+if [ ! -f ../config/project-scratch-def.json ]; then
+	if [ ! -f config/project-scratch-def.json ]; then
+		echo "Please try running this from the /scripts folder or the project root."
+		>&2 echo "config/project-scratch-def.json not found"
+		exit 1
+	else
+		echo "Creating scratch org with alias $1..."
+		orgCreation=`sfdx force:org:create -f config/project-scratch-def.json -s -a "$1"`
+	fi
+else
+	echo "Creating scratch org with alias $1..."
+	orgCreation=`sfdx force:org:create -f ../config/project-scratch-def.json -s -a "$1"`
+fi
+#if [[ "$orgCreation" =~ ^ERROR.* ]]; then
+#	echo $orgCreation
+#	exit 1
+#else
+#	echo "$orgCreation" =~ &ERROR.*
+#fi
 echo "Pushing local source to scratch org..."
 sfdx force:source:push
 echo "Assigning permission set to user..."
